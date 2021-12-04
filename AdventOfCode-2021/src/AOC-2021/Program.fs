1bd6a7ad1c78e2462892fbbbefb4de5d6facdf61
John Maynard <maynard.john.e@gmail.com>
2021-12-04 13:55:55 -0500

    Initial Commit.

---
+++ AdventOfCode-2021/src/AOC-2021/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+// For more information see https://aka.ms/fsharp-console-apps
+let lines = System.IO.File.ReadLines(".\Data\input2.txt")
+
+type Coordinate = { Position: int; Depth: int }
+
+let parsePositionData (rowStringData: string): Coordinate = 
+    let columns = rowStringData.Split(' ')
+    match columns[0] with
+    | "forward" -> { Position = Convert.ToInt32(columns[1]); Depth = 0 }
+    | "down"    -> { Position = 0; Depth = Convert.ToInt32(columns[1]) }
+    | "up"      -> { Position = 0; Depth = -(Convert.ToInt32(columns[1])) }
+    | _         -> { Position = 0; Depth = 0 }
+
+// Parse all data
+let allCoordinates = lines |> Seq.map(fun(x) -> parsePositionData(x))
+
+// Show Data
+let showData x = printfn "%A" <| x
+allCoordinates |> Seq.iter(showData)
+
+// Reduce all coordinates (using sumPosotionData function)
+let sumPositionData x y = { Position = x.Position + y.Position; Depth = x.Depth + y.Depth }
+let finalPosition = allCoordinates |> Seq.reduce(sumPositionData)
+
+// Multiply Position by Depth.
+let positionProduct = finalPosition.Position * finalPosition.Depth
+
+// Display result
+printfn "\nFinal Position: %A" <| finalPosition
+printfn "\nPosition Product: %s" <| (positionProduct).ToString("N0")
