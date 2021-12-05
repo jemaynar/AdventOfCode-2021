f2ec4d17de3ad51521cb07cb1c465903fc1e6864
John Maynard <maynard.john.e@gmail.com>
2021-12-04 21:52:23 -0500

    Making the addition of further AOC Days less sloppy.

---
+++ AdventOfCode-2021/src/AOC-2021/Day2.fs
@@ -0,0 +1,38 @@
+module Day2
+    open System
+
+    type CoordinateType = { Position: int; Depth: int }
+
+    module Coordinate =
+        let parsePositionData (rowStringData: string): CoordinateType = 
+            let columns = rowStringData.Split(' ')
+            match columns[0] with
+            | "forward" -> { Position = Convert.ToInt32(columns[1]); Depth = 0 }
+            | "down"    -> { Position = 0; Depth = Convert.ToInt32(columns[1]) }
+            | "up"      -> { Position = 0; Depth = -(Convert.ToInt32(columns[1])) }
+            | _         -> { Position = 0; Depth = 0 }
+
+        let sumCoordinate x y = { Position = x.Position + y.Position; Depth = x.Depth + y.Depth }
+
+        let positionProduct x = x.Position * x.Depth
+
+        let displayCoordinate x = printfn "%A" <| x
+
+    let Execute =
+        let lines = System.IO.File.ReadLines(".\Data\input2.txt")
+
+        // Parse all data
+        let allCoordinates = lines |> Seq.map(fun(x) -> Coordinate.parsePositionData(x))
+
+        // Show Data
+        allCoordinates |> Seq.iter(Coordinate.displayCoordinate)
+
+        // Reduce all coordinates (using sumPosotionData function)
+        let finalPosition = allCoordinates |> Seq.reduce(Coordinate.sumCoordinate)
+
+        // Multiply Position by Depth.
+        let positionProduct = finalPosition.Position * finalPosition.Depth
+
+        // Display result
+        printfn "\nFinal Position: %A" <| finalPosition
+        printfn "\nPosition Product: %s" <| positionProduct.ToString("N0")
