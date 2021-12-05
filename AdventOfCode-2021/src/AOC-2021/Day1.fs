c4bda1afb81958ccaca85927c4c01400ba381add
John Maynard <maynard.john.e@gmail.com>
2021-12-05 14:12:23 -0500

    Mode Day 1 sum function into module Part1.

--- AdventOfCode-2021/src/AOC-2021/Day1.fs
+++ AdventOfCode-2021/src/AOC-2021/Day1.fs
@@ -1,16 +1,19 @@
 module Day1
     open System
 
+    module Part1 =
+        let sumData (rows): int = 
+            rows |> Seq.map(fun(x:string) -> Convert.ToInt32(x)) 
+                |> Seq.pairwise 
+                |> Seq.map(fun (previous, next) -> if next > previous then 1 else 0)
+                |> Seq.sum
+
     let Execute =
         let lines = System.IO.File.ReadLines(".\Data\input1.txt")
 
         // Show Data
         // lines |> Seq.iter(fun(x) -> printfn "%s" x)
 
-        let depths = 
-            lines |> Seq.map(fun(x) -> Convert.ToInt32(x)) 
-                |> Seq.pairwise 
-                |> Seq.map(fun (previous, next) -> if next > previous then 1 else 0)
-                |> Seq.sum
+        let depths = Part1.sumData <| lines
 
         printfn "Day 1 Result: \n\n%A" <| depths
