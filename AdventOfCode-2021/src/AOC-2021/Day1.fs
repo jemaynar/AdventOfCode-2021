0724dac2a51f50ae80b64bb948432152c211fe07
John Maynard <maynard.john.e@gmail.com>
2021-12-05 19:19:50 -0500

    Adding Day 1 part 2 code.

--- AdventOfCode-2021/src/AOC-2021/Day1.fs
+++ AdventOfCode-2021/src/AOC-2021/Day1.fs
@@ -2,10 +2,18 @@ module Day1
     open System
 
     module Part1 =
-        let sumData (rows): int = 
-            rows |> Seq.map(fun(x:string) -> Convert.ToInt32(x)) 
+        let sumData (rows: seq<string>): int = 
+            rows |> Seq.map(fun(x) -> Convert.ToInt32(x)) 
                 |> Seq.pairwise 
                 |> Seq.map(fun (previous, next) -> if next > previous then 1 else 0)
+                |> Seq.sum
+
+    module Part2 =
+        let sumData (rows: seq<string>): int =
+            rows |> Seq.map(Convert.ToInt32)
+                |> Seq.windowed 3
+                |> Seq.pairwise
+                |> Seq.map(fun (previous, next) -> if Seq.sum(previous) < Seq.sum(next) then 1 else 0)
                 |> Seq.sum
 
     let Execute =
@@ -16,4 +24,9 @@ module Day1
 
         let depths = Part1.sumData <| lines
 
-        printfn "Day 1 Result: \n\n%A" <| depths
+        printfn "Day 1 / Part 1 Result: \n\n%A" <| depths
+
+        let depths = Part2.sumData <| lines
+
+        printfn "Day 1 / Part 2 Result: \n\n%A" <| depths
+        
