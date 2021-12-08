35a576a6c4fee153fcbde18810928802dd8e484a
John Maynard <maynard.john.e@gmail.com>
2021-12-08 08:07:50 -0500

    The beginnings of Day 3 solution.

--- AdventOfCode-2021/src/AOC-2021/Day3.fs
+++ AdventOfCode-2021/src/AOC-2021/Day3.fs
@@ -1,2 +1,27 @@
 module Day3
     open System
+
+    let Execute: unit =
+        let lines = System.IO.File.ReadLines(".\Data\input3.txt")
+
+        let mappedLines = 
+            lines 
+            |> Seq.map(fun line -> line.ToCharArray())
+            |> Seq.map(fun (charArray) -> 
+                charArray 
+                    |> Seq.map(fun x -> 
+                        match x with 
+                            | '1' -> 1uy 
+                            | _ -> 0uy ))
+
+        // Thank you stack overflow!
+        let pivotedLines =
+            mappedLines 
+                |> Seq.collect Seq.indexed
+                |> Seq.groupBy fst
+                |> Seq.map (snd >> Seq.map snd)
+            
+        printfn "\n\nDay 3 Result:\n"
+
+        // Show Data
+        pivotedLines |> Seq.iter(fun(x) -> printfn "%A" x)
--- AdventOfCode-2021/src/AOC-2021/Program.fs
+++ AdventOfCode-2021/src/AOC-2021/Program.fs
@@ -1,8 +1,10 @@
 ﻿open System
 open Day2
+open Day3
 
 [<EntryPoint>]
 let main argv =
     Day1.Execute
     Day2.Execute
+    Day3.Execute
     0
