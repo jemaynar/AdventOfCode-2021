ba285a89e5745af76041effc7eeffa38390a2b59
John Maynard <maynard.john.e@gmail.com>
2021-12-08 08:15:03 -0500

    Reorganize Day3 code so far.

--- AdventOfCode-2021/src/AOC-2021/Day3.fs
+++ AdventOfCode-2021/src/AOC-2021/Day3.fs
@@ -1,25 +1,30 @@
 module Day3
     open System
 
-    let Execute: unit =
-        let lines = System.IO.File.ReadLines(".\Data\input3.txt")
-
-        let mappedLines = 
+    module Part1 =
+        let mapLines (lines: seq<string>) =
             lines 
-            |> Seq.map(fun line -> line.ToCharArray())
-            |> Seq.map(fun (charArray) -> 
-                charArray 
-                    |> Seq.map(fun x -> 
-                        match x with 
-                            | '1' -> 1uy 
-                            | _ -> 0uy ))
+                |> Seq.map(fun (line) -> line.ToCharArray())
+                |> Seq.map(fun (charArray) -> 
+                    charArray 
+                        |> Seq.map(fun x -> 
+                            match x with 
+                                | '1' -> 1uy 
+                                | _ -> 0uy ))
 
-        // Thank you stack overflow!
-        let pivotedLines =
+        let pivotLines mappedLines =
             mappedLines 
                 |> Seq.collect Seq.indexed
                 |> Seq.groupBy fst
                 |> Seq.map (snd >> Seq.map snd)
+
+    let Execute: unit =
+        let lines = System.IO.File.ReadLines(".\Data\input3.txt")
+
+        let mappedLines = Part1.mapLines lines
+
+        // Thank you stack overflow!
+        let pivotedLines = Part1.pivotLines mappedLines
             
         printfn "\n\nDay 3 Result:\n"
 
