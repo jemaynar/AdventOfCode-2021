a9a7d4a636608a8fc5341f39f97e1f8ff7a09135
John Maynard <maynard.john.e@gmail.com>
2022-02-09 05:56:35 -0500

    Add test for pivotSequenceOfBitArrays.

--- AdventOfCode-2021/src/UnitTests/Day3Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day3Tests.fs
@@ -19,3 +19,30 @@ module UnitTests.Day3Tests
 
         let expectedResult = seq { yield [| for i in 1 .. 12 -> 1 |] }
         Assert.Equal<IEnumerable<int[]>>(result, expectedResult)
+        
+    [<Fact>]
+    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 0 |] } -> { "000000000000" }`` () =
+        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 0 |] } 
+        
+        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
+
+        let expectedResult = seq { yield "000000000000" }
+        Assert.Equal<IEnumerable<string>>(result, expectedResult)
+        
+    [<Fact>]
+    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 1 |] } -> { "111111111111" }`` () =
+        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 1 |] } 
+        
+        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
+
+        let expectedResult = seq { yield "111111111111" }
+        Assert.Equal<IEnumerable<string>>(result, expectedResult)
+        
+    [<Fact>]
+    let ``pivotSequenceOfBitArrays: an array of 12 0's is transformed to 12 sequences containing a 0 followed by 1`` () =
+        let sequenceOfArrayOfInts = seq { for x in 0 .. 1 -> [| for i in 1 .. 12 -> x |] } 
+        
+        let result = Day3.pivotSequenceOfBitArrays sequenceOfArrayOfInts
+        
+        let expectedResult = seq { for x in 1 .. 12 -> seq { 0; 1 } }
+        Assert.Equal<IEnumerable<IEnumerable<int>>>(result, expectedResult)
