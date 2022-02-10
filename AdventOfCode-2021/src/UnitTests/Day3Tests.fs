9f0e7b8e27f2ce8a4fc7d75f6b78ec6e77a3c655
John Maynard <maynard.john.e@gmail.com>
2022-02-10 05:43:43 -0500

    Changing order of tests to match order they appear in test file.

--- AdventOfCode-2021/src/UnitTests/Day3Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day3Tests.fs
@@ -1,6 +1,24 @@
 module UnitTests.Day3Tests
     open System.Collections.Generic
     open Xunit
+                
+    [<Fact>]
+    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 0 |] } -> { "000000000000" }`` () =
+        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 0 |] } 
+        
+        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
+
+        let expectedResult = seq { yield "000000000000" }
+        Assert.Equal<IEnumerable<string>>(result, expectedResult)
+        
+    [<Fact>]
+    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 1 |] } -> { "111111111111" }`` () =
+        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 1 |] } 
+        
+        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
+
+        let expectedResult = seq { yield "111111111111" }
+        Assert.Equal<IEnumerable<string>>(result, expectedResult)
     
     [<Fact>]
     let ``toSequenceOfBitArrays: { "000000000000" } -> { [| for i in 1 .. 12 -> 0 |] }`` () =
@@ -19,25 +37,7 @@ module UnitTests.Day3Tests
 
         let expectedResult = seq { yield [| for i in 1 .. 12 -> 1 |] }
         Assert.Equal<IEnumerable<int[]>>(result, expectedResult)
-        
-    [<Fact>]
-    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 0 |] } -> { "000000000000" }`` () =
-        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 0 |] } 
-        
-        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
-
-        let expectedResult = seq { yield "000000000000" }
-        Assert.Equal<IEnumerable<string>>(result, expectedResult)
-        
-    [<Fact>]
-    let ``toArrayOfBinaryStrings: { [| for in in 1 .. 12 -> 1 |] } -> { "111111111111" }`` () =
-        let sequenceOfArrayOfInts = seq { yield [| for i in 1 .. 12 -> 1 |] } 
-        
-        let result = Day3.toArrayOfBinaryStrings sequenceOfArrayOfInts
-
-        let expectedResult = seq { yield "111111111111" }
-        Assert.Equal<IEnumerable<string>>(result, expectedResult)
-        
+                
     [<Fact>]
     let ``pivotSequenceOfBitArrays: a sequence 2 elements the first of 12 0's and the second of 12 1's is transformed to 12 arrays containing [| 0; 1 |]`` () =
         let sequenceOfArrayOfInts = seq { for x in 0 .. 1 -> [| for i in 1 .. 12 -> x |] } 
