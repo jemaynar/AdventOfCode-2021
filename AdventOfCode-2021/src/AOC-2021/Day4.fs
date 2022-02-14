cedddf51533a7eee79b2b10ef3d08a9c1ca5ab9c
John Maynard <maynard.john.e@gmail.com>
2022-02-14 05:48:57 -0500

    Setting up for problem Day4.

---
+++ AdventOfCode-2021/src/AOC-2021/Day4.fs
@@ -0,0 +1,7 @@
+module Day4
+    module Part1 =
+        let Execute: unit =
+            printfn "\n\nDay 4 / Part 1 Result:\n"
+        
+    let Execute: unit =
+        Part1.Execute
--- AdventOfCode-2021/src/AOC-2021/Program.fs
+++ AdventOfCode-2021/src/AOC-2021/Program.fs
@@ -3,4 +3,5 @@ let main argv =
     Day1.Execute
     Day2.Execute
     Day3.Execute
+    Day4.Execute
     0
