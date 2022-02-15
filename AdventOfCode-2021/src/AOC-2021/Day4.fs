aa45e2f39886c0e4e4ef28fdbbe3cfcbb3b3f27f
John Maynard <maynard.john.e@gmail.com>
2022-02-15 05:45:04 -0500

    Day4 - Add code necessary to parse drawn numbers for bingo.

--- AdventOfCode-2021/src/AOC-2021/Day4.fs
+++ AdventOfCode-2021/src/AOC-2021/Day4.fs
@@ -1,9 +1,20 @@
 module Day4
     type BingoCell = { IsSelected: bool; Value: byte }
     
+    let drawnNumbers (inputLines: seq<string>): int[] =
+        let firstLine = inputLines |> Seq.head
+    
+        firstLine.Split ',' |> Array.map(int)
+    
     module Part1 =
         let Execute: unit =
-            printfn "\n\nDay 4 / Part 1 Result:\n"
+            printfn "\nDay 4 / Part 1 Result:\n"
+        
+            let lines = Common.getData ".\Data\input4.txt"
+                
+            let picks = drawnNumbers <| lines
+            
+            printfn "DrawnNumbers: %A" <| picks
         
     let Execute: unit =
         Part1.Execute
