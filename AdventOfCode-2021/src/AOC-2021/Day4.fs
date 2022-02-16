d107207332215ab0daf78dc5a89eaa6952908e40
John Maynard <maynard.john.e@gmail.com>
2022-02-16 07:03:52 -0500

    Day4 - Changed return type of getGameBoards.

--- AdventOfCode-2021/src/AOC-2021/Day4.fs
+++ AdventOfCode-2021/src/AOC-2021/Day4.fs
@@ -15,7 +15,10 @@ module Day4
                 |> Array.ofSeq
                 
     let getGameBoards (inputLines: seq<string>) =
-        array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]]
+        let gameBoards = seq {
+            array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]];
+        }
+        gameBoards
                 
     module Part1 =
         let Execute: unit =
--- AdventOfCode-2021/src/UnitTests/Day4Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day4Tests.fs
@@ -47,5 +47,5 @@ module UnitTests.Day4Tests
         
         let result = Day4.getGameBoards <| inputLines
         
-        Assert.Equal(array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]], result)
+        Assert.Equal<seq<int[,]>>(seq { array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]]; }, result)
         
