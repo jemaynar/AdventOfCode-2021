88c04ce089110eaf8d836544bdda7e5e36c30db5
John Maynard <maynard.john.e@gmail.com>
2022-02-16 06:54:35 -0500

    Day4Tests.fs - getGameboards first test, completely hard coded.  The TDD way - simplest implementation that passes test.

--- AdventOfCode-2021/src/AOC-2021/Day4.fs
+++ AdventOfCode-2021/src/AOC-2021/Day4.fs
@@ -14,6 +14,9 @@ module Day4
                 |> Seq.choose(fun n -> match Int32.TryParse(n) with | true, n -> Some n | false, _ -> None)
                 |> Array.ofSeq
                 
+    let getGameBoards (inputLines: seq<string>) =
+        array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]]
+                
     module Part1 =
         let Execute: unit =
             printfn "\nDay 4 / Part 1 Result:\n"
@@ -23,6 +26,10 @@ module Day4
             let picks = getDrawnNumbers <| lines
             
             printfn "DrawnNumbers: %A" <| picks
+            
+            let gameBoards = getGameBoards <| lines
+            
+            printfn "GameBoards: %A" <| gameBoards
         
     let Execute: unit =
         Part1.Execute
--- AdventOfCode-2021/src/UnitTests/Day4Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day4Tests.fs
@@ -1,4 +1,4 @@
-module Day4Tests
+module UnitTests.Day4Tests
     open Xunit
 
     [<Fact>]
@@ -32,3 +32,20 @@ module Day4Tests
         let result = Day4.getDrawnNumbers <| inputLines
         
         Assert.Equal<int[]>([| 1 |], result)
+        
+    [<Fact>]
+    let ``getGameBoards: when input contains single game board then result is Array2D containing game board values`` () =
+        let inputLines = seq {
+            "1,2,3,4,5";
+            "";
+            "22 13 17 11  0";
+            " 8  2 23  4 24";
+            "21  9 14 16  7";
+            " 6 10  3 18  5";
+            " 1 12 20 15 19"
+        }
+        
+        let result = Day4.getGameBoards <| inputLines
+        
+        Assert.Equal(array2D [[ 22; 13; 17; 11; 0 ]; [8; 2; 23; 4; 24]; [21; 9; 14; 16; 7]; [6; 10; 3; 18; 5]; [1; 12; 20; 15; 19]], result)
+        
