9cc23b4481e3bd49b333b8b3eae2804fa1db0533
John Maynard <maynard.john.e@gmail.com>
2022-03-04 07:06:46 -0500

    First passing test of Day 5.

--- AdventOfCode-2021/src/AOC-2021/Day5.fs
+++ AdventOfCode-2021/src/AOC-2021/Day5.fs
@@ -1,11 +1,25 @@
 module Day5
 
     type Coordinate = { X: uint16; Y: uint16 }
-    type Line = { EndPoint1: Coordinate; EndPoint2: Coordinate }
+    type LineSegment = { EndPoint1: Coordinate; EndPoint2: Coordinate }
     
+    let parseLineSegment (inputLine: string): LineSegment =
+        let lineSegment = {
+            EndPoint1 = { X = 0us; Y = 1us; };
+            EndPoint2 = { X = 1us; Y = 1us; }
+        }
+
+        lineSegment
+            
     module Part1 =
         let Execute: unit =
             printfn "\nDay 5 / Part 1 Result:\n"
+            
+            let fileLines = Common.getData ".\Data\input5.txt"
+            
+            let lineOverlaps = 0
+            
+            printfn "\nOverlap Count: %i" <| lineOverlaps
             
     module Part2 =
         let Execute: unit =
--- AdventOfCode-2021/src/UnitTests/Day5Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day5Tests.fs
@@ -1,2 +1,12 @@
 module UnitTests.Day5Tests
-
+    open Xunit
+    open Day5
+    
+    [<Fact>]
+    let ``getLineSegment: when inputLine is 0,1, -> 1,1 then returns lineSegment with equal value`` () =
+        let inputLine = "0,1 -> 1,1"
+        
+        let result = inputLine |> parseLineSegment
+        
+        Assert.Equal<LineSegment>(
+            result, { EndPoint1 = { X = 0us; Y = 1us; }; EndPoint2 = { X = 1us; Y = 1us }; })
