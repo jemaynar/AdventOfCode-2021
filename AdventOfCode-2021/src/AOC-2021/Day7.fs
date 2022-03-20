3cb175116a7a89d6cef8ff8324f9f566ec9fc080
John Maynard <maynard.john.e@gmail.com>
2022-03-20 07:59:40 -0400

    Day 7 - parseCrabs build out.

--- AdventOfCode-2021/src/AOC-2021/Day7.fs
+++ AdventOfCode-2021/src/AOC-2021/Day7.fs
@@ -1,4 +1,9 @@
 module Day7
+    type Crab = { HorizontalPosition: uint }
+
+    let parseCrabs inputLine =
+        None
+
     module Part1 =
         let Execute: unit =
             printfn "\nDay 7 / Part 1 Result:\n"
--- AdventOfCode-2021/src/UnitTests/Day7Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day7Tests.fs
@@ -1,2 +1,11 @@
 module UnitTests.Day7Tests
+    open Xunit
+    open Day7
 
+    [<Fact>]
+    let rec ``parseCrabs: when line is empty then returns Option None`` () =
+        let inputLine = System.String.Empty
+
+        let result = inputLine |> parseCrabs
+
+        Assert.Equal<Option<seq<Crab>>>(None, result)
