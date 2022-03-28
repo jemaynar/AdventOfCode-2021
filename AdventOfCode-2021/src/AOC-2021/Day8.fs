b87434f6fce31481e6d63736ed0ef2266bff476f
John Maynard <maynard.john.e@gmail.com>
2022-03-28 08:43:28 -0400

    Day 8 - Adding map between length of digits and the digit it represents.

--- AdventOfCode-2021/src/AOC-2021/Day8.fs
+++ AdventOfCode-2021/src/AOC-2021/Day8.fs
@@ -1,6 +1,14 @@
 module Day8
 
     module Part1 =
+        let lengthToNumberMap =
+            Map[
+                (2, 1)
+                (3, 7)
+                (4, 4)
+                (7, 8)
+            ]
+
         let Execute: unit =
             printfn "\nDay 8 / Part 1 Result:"
 
