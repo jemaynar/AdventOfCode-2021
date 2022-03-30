1ad21c6b04232e042d0db26bb102663e32bf0afd
John Maynard <maynard.john.e@gmail.com>
2022-03-30 07:56:31 -0400

    Day 8 / Part 1 - Add confidence boosting test.

--- AdventOfCode-2021/src/UnitTests/Day8Tests.fs
+++ AdventOfCode-2021/src/UnitTests/Day8Tests.fs
@@ -78,3 +78,27 @@ module UnitTests.Day8Tests
                 |> Part1.getKnownDigitCount
 
         Assert.Equal<int>(4, result)
+
+    [<Fact>]
+    let ``Part1.getKnownDigitCount: when lines from sample problem then returns 26`` () =
+        let inputLines =
+            seq {
+                "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+                "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
+                "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"
+                "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+                "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
+                "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
+                "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
+                "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
+                "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
+                "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+            }
+
+        let result =
+            inputLines
+                |> Seq.map(parseLine)
+                |> Seq.choose id
+                |> Part1.getKnownDigitCount
+
+        Assert.Equal<int>(26, result)
